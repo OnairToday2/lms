b3ccaf2b9af58f23f8e679befd4822f5866d8ae9
VanDuy <duylangtu931@gmail.com>
2025-10-14 16:42:13 +0700

    refactor menu configuration to use PATH constants for routing

---
+++ src/constants/path.contstants.ts
@@ -0,0 +1,17 @@
+
+export const PATHS = {
+    ROOT: '/',
+    DASHBOARD: '/dashboard',
+    ANALYTIC: '/analytic',
+    ORGANIZATION: '/organization',
+    DEPARTMENTS: '/departments',
+    EMPLOYEES: '/employees',
+    EMPLOYEES_ID: (id: string = ':id') => `/employees/${id}`,
+    CREATE_EMPLOYEE: '/employees/create',
+    IMPORT_EMPLOYEES: '/employees/import',
+    ROLES: '/roles',
+    ROLES_ID: (id: string = ':id') => `/roles/${id}`,
+    CREATE_ROLE: '/roles/create',
+    CLASSROOMS: '/class-room',
+    CREATE_CLASSROOM: '/class-room/create',
+}
