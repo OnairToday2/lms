af071d3c683bc8e107bdd0c7eebbe0775447eff9
duyzuy <vutruongduy2109@gmail.com>
2025-11-04 12:18:52 +0700

    update menu config

--- src/app/(organization)/class-room/manage/[platform]/layout.tsx
+++ src/app/(organization)/class-room/manage/[platform]/layout.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 import { CLASS_ROOM_PLATFORM } from "@/constants/class-room.constant";
+import { PATHS } from "@/constants/path.contstants";
 interface ClassRoomPlatformLayoutProps {
   params: Promise<{
     platform: (typeof CLASS_ROOM_PLATFORM)[keyof typeof CLASS_ROOM_PLATFORM];
@@ -11,7 +12,7 @@ const CLASS_ROOM_PREFIX_PATHS = [CLASS_ROOM_PLATFORM.ONLINE, CLASS_ROOM_PLATFORM
 export default async function ClassRoomPlatformLayout({ children, params }: ClassRoomPlatformLayoutProps) {
   const { platform } = await params;
   if (!CLASS_ROOM_PREFIX_PATHS.includes(platform)) {
-    redirect("/dashboard");
+    redirect(PATHS.CLASSROOMS.ROOT);
   }
 
   return children;
